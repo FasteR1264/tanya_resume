345171e666f272bdfdf018c2de3f09edcc7ed831
Andrey <petrovandrey300@gmail.com>
2022-08-09 22:24:54 +0300

    Add section about

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector('.menu'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    menuItem = document.querySelectorAll('.menu__item');
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('menu_active');
     hamburger.classList.toggle('hamburger_active');
+});
+
+menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+        menu.classList.toggle('menu_active');
+        hamburger.classList.toggle('hamburger_active');
+    });
 });
